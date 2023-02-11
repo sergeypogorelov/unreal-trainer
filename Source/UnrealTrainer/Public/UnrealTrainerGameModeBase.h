23ae715012a6279868438bef6e1af6dc5722cfd4
Sergey Pogorelov <sergey.pogorelov.1993@yandex.ru>
2023-02-11 17:18:48 +0300

    Set up empty UE project

---
+++ Source/UnrealTrainer/Public/UnrealTrainerGameModeBase.h
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTrainerGameModeBase.generated.h"
+
+UCLASS()
+class UNREALTRAINER_API AUnrealTrainerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
